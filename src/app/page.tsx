4c44a270f80bc91634db8c480ded4aa3a327c096
Josh Ward <joshwardcodes@gmail.com>
2025-10-12 09:53:20 -0700

    add temporary console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SignInButton from '@/components/SignInButton';
 
 export default function Home() {
+  console.log('url: ', process.env.BETTER_AUTH_URL);
   return (
     <div>
       <SignInButton />
