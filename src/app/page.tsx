aff52452261e4cd9ceb472c95441428c89f2b985
Josh Ward <joshwardcodes@gmail.com>
2025-10-12 09:58:45 -0700

    swap back to other url and remove temporary logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import SignInButton from '@/components/SignInButton';
 
 export default function Home() {
-  console.log('url: ', process.env.BETTER_AUTH_URL);
   return (
     <div>
       <SignInButton />
