9f90521836b85190e66472b499cd04df1308c4e3
Josh Ward <joshwardcodes@gmail.com>
2025-10-14 16:47:56 -0700

    adjust padding

--- src/app/MainHeader.tsx
+++ src/app/MainHeader.tsx
@@ -2,7 +2,7 @@ import { HeaderActions } from './HeaderActions';
 
 export function MainHeader() {
   return (
-    <header className="flex items-center justify-between gap-6 p-4 md:p-6">
+    <header className="flex items-center justify-between gap-6 p-2 md:p-6">
       <div className="flex grow items-center gap-4 md:gap-6">
         <h1 className="text-slate-12 text-4xl font-serif font-medium">Tavern Master</h1>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           <div className="from-violet-8 dark:from-violet-4 absolute top-0 right-0 left-0 -z-50 h-[300px] w-full bg-gradient-to-b to-transparent" />
           <MainHeader />
 
-          <div className="grow flex flex-col items-center px-3">
+          <div className="grow flex flex-col items-center px-2 md:px-6">
             <div className="w-full md:max-w-[80rem]">{children}</div>
           </div>
 
